d77435f4c7554f0ca58c2c2e3c6db042e1cf9584
wonlayhwan <37726078+wonlayhwan@users.noreply.github.com>
2018-10-26 15:34:51 +0800

    table static way
    table static way

--- src/app/order-completeness-summary/order-completeness-summary.component.ts
+++ src/app/order-completeness-summary/order-completeness-summary.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-completeness-summary.component.css']
 })
 export class OrderCompletenessSummaryComponent implements OnInit {
-
+  dataSet = [
+    {
+      key    : '1',
+      name   : 'John Brown',
+      age    : 32,
+      address: 'New York No. 1 Lake Park'
+    },
+    {
+      key    : '2',
+      name   : 'Jim Green',
+      age    : 42,
+      address: 'London No. 1 Lake Park'
+    },
+    {
+      key    : '3',
+      name   : 'Joe Black',
+      age    : 32,
+      address: 'Sidney No. 1 Lake Park'
+    }
+  ];
+  
   constructor() { }
 
   ngOnInit() {
   }
-
 }
